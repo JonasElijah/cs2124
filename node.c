ed5446f48932466e10c619ac31b0df2bf3119152
Jonas Elijah Icban <jonaselijah11@gmail.com>
2022-09-06 13:26:45 -0500

    dioasfkj

--- node.c
+++ node.c
@@ -1,5 +1,17 @@
 #include "node.h"
-#include "Utilities.h"
+//#include "Utilities.h"
+#define COMMENT_MARKER '#'
+void commentOut(char* x)
+{
+    char* comment = strchr(x,COMMENT_MARKER);//points to the address to where # is located,returns null is not found
+    if(comment != 0)
+     {
+        size_t len = strlen(comment);//gets the length of comment and stores it in len
+        memset(comment, '\0' ,len);//sets all characters of comment to NULL
+        *comment = '\n';
+    }
+}
+
 
 void createNode(char* buffer, Node x[],int y)
 {
