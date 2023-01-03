6298cab11f24a9e06b3394c7ecf38359cc076cb6
JonasElijah <89656029+JonasElijah@users.noreply.github.com>
2023-01-03 14:18:36 -0600

    Add files via upload

---
+++ Util.h
@@ -0,0 +1,15 @@
+#ifndef UTIL_HEADER
+#define UTIL_HEADER
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define COMMENT_MARKER '#'
+
+void commentOut(char* x);
+/*int findIndex(char buff[], int val);*/
+
+
+
+#endif
